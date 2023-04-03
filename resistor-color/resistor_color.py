ae1eca1c2ea7e3145f219cd1196555d4d181cd28
noah-clements <noahclements@gmail.com>
2023-04-03 15:48:25 -0400

    passes tests

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,8 @@
+RESISTOR_COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+def color_code(color):
+    return RESISTOR_COLORS.index(color)
+
+
+def colors():
+    return RESISTOR_COLORS
