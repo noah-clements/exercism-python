ada66b4d96aab37ce0790ac127bd265e3bbdb5ff
noah-clements <noahclements@gmail.com>
2023-04-03 15:45:57 -0400

    misspelled resistor

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,8 +1,8 @@
-RESISTER_COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+RESISTOR_COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
 def value(colors):
     if len(colors) < 2:
         raise ValueError("Need two colors")
-    return int(str(RESISTER_COLORS.index(colors[0])) + str(RESISTER_COLORS.index(colors[1])))
+    return int(str(RESISTOR_COLORS.index(colors[0])) + str(RESISTOR_COLORS.index(colors[1])))
 
 
